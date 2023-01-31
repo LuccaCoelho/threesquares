a2a7e5a74498a871ebc8b82f963ba48c2665dec5
Lenovo <Lenovo@DESKTOP-17V744A>
2023-01-31 15:40:12 -0500

    Adicionar arquivos de projeto.

---
+++ threesquares/threesquares.py
@@ -0,0 +1,23 @@
+#input 1
+square = (float(input("Input 1: ")))
+
+z = square * square
+
+print(square ,"square is", z)
+
+#input 2
+square = (float(input("Input 2: ")))
+
+z = square * square
+
+print(square ,"square is", z)
+
+#input 3
+square = (float(input("Input 3: ")))
+
+z = square * square
+
+print(square ,"square is", z)
+
+
+#Vinccius Florindo Coelho
